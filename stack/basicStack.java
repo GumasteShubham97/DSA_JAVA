48673d1fac77d3497124dfd56de6f2f06e5d4f2f
shubham_gumaste <shubham.gumaste@zycus.com>
2024-11-10 21:04:09 +0530

    first dsa code j

--- stack/basicStack.java
+++ stack/basicStack.java
@@ -10,7 +10,6 @@ public class basicStack {
         s.push(2); // Pushing 2 to the top
         s.push(3); // Pushing 3 to the top
         s.push(4); // Pushing 4 to the top
-        s.push(5); // Pushing 5 to the top
 
         // Printing in reverse order
         while (!s.isEmpty()) {
